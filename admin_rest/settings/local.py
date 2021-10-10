b4dfaa180e9cceca930399eaed714dae2ae1e882
Juan Andres Lopez <j_andreslm@hotmail.com>
2021-10-09 21:34:16 -0500

    Actualizado proyecto ver nota 0.5.2

--- admin_rest/settings/local.py
+++ admin_rest/settings/local.py
@@ -1,4 +1,6 @@
 from .base import *
+import dj_database_url
+from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -8,9 +10,15 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=config('DATABASE_URL'))
     }
 }
